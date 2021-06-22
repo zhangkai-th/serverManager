e4fd3be00e43c6bea84775b851d5ec9769bae360
zhangkai-th <837861802@qq.com>
2021-06-22 21:09:33 +0800

    添加数组元素23

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("hello world");
+	    int data[]=new int[3];
+	    data[0]=1;
+	    data[1]=2;
+        System.out.println(data.length);
+        System.out.println(Arrays.stream(data).count());
     }
 }
